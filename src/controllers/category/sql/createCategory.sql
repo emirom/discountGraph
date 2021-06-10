b2d5ff58e338bd1aa7d0b0df9bb23cbf2500dd8b
amir meyari <forsite73@gmail.com>
2021-06-10 18:59:46 +0430

    fixed category sqls

--- src/controllers/category/sql/createCategory.sql
+++ src/controllers/category/sql/createCategory.sql
@@ -1,6 +1,8 @@
-CREATE TABLE IF NOT EXISTS category (
-      id INT NOT NULL PRIMARY KEY,
-      name VARCHAR(45) NOT NULL,
-      discount NUMERIC(3,2)
-)
+  CREATE TABLE IF NOT EXISTS categories (
+        id SERIAL NOT NULL PRIMARY KEY,
+        name VARCHAR(45) NOT NULL,
+        parent_id INTEGER,
+        discount INTEGER,
+        graph_discount INTEGER
+      );
 
